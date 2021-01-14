0a4cbdf4b1f56b62b8a2250b9d7ced8b88075499
Bart <bartlomiej.janik@hotmail.fr>
2021-01-14 15:01:40 +0100

    DATA and funcDispatcher are now arrays + added DOMContentLoaded Event

--- Bartify/script.js
+++ Bartify/script.js
@@ -1,101 +1,94 @@
 
-DATA = {
-    1: {
+DATA = [
+    {
         "title": "Master Of Puppets",
         "artist": "Metallica",
         "song": "media/master-of-puppets-remastered.mp3",
         "cover": "img/master-of-puppets.webp",
         "year": 1986,
     },
-    2: {
+    {
         "title": "Killing In The Name",
         "artist": "Rage Against The Machine",
         "song": "media/killing-in-the-name.mp3",
         "cover": "img/ratm.jpg",
         "year": 1992,
-
     },
-    3: {
-        "title": "Nico",
-        "artist": "In Vino Veritas",
+    {
+        "title": "In Vino Veritas",
+        "artist": "Nico",
         "song": "media/in-vino-veritas.mp3",
         "cover": "img/nico.jpg",
         "year": 2020,
-
     },
-    4: {
+    {
         "title": "Smells Like Teen Spirit",
         "artist": "Nirvana",
         "song": "media/smells-like-teen-spirit.mp3",
         "cover": "img/nevermind.jpg",
         "year": 1991,
-
     },
-    5: {
+    {
         "title": "In The End",
         "artist": "Linkin Park",
         "song": "media/in-the-end.mp3",
         "cover": "img/hybrid-theory.jpg",
         "year": 2000,
-
     },
-    6: {
+    {
         "title": "I Got 5 On It",
         "artist": "The Luniz",
         "song": "media/i-got-5-on-it.mp3",
         "cover": "img/operation-stackola.jpg",
         "year": 1995,
-
     },
-    7: {
+    {
         "title": "You Can't Bring Me Down",
         "artist": "Suicidal Tendencies",
         "song": "media/you-cant-bring-me-down.mp3",
         "cover": "img/lcr.jpg",
         "year": 1992,
-
     },
-    8: {
+    {
         "title": "Jammin",
         "artist": "Bob Marley & The Wailers",
         "song": "media/jammin.mp3",
         "cover": "img/exodus.jpg",
         "year": 1977,
-
     },
-    9: {
+    {
         "title": "The Kids Aren't Alright",
         "artist": "The Offspring",
         "song": "media/the-kids-arent-alright.mp3",
         "cover": "img/americana.jpeg",
         "year": 1998,
     },
-    10: {
+    {
         "title": "Chop Suey",
         "artist": "System Of A Down",
         "song": "media/chop-suey.mp3",
         "cover": "img/toxicity.jpg",
         "year": 2001,
     },
-    11: {
+    {
         "title": "Adagio For Strings",
         "artist": "Tiesto",
         "song": "media/adagio-for-strings.mp3",
         "cover": "img/adagio.jpg",
         "year": 2004,
     }
-}
+]
 
 const generateTable = () => {
 
     const tbody = document.getElementsByTagName('tbody')[0]
 
-    const funcDispatcher = {
-        0: (td, index) => {
+    const funcDispatcher = [
+        (td, index) => {
             let audio = document.createElement('audio')
             let button = document.createElement('button')
 
-            audio.src = DATA[index + 1].song
+            audio.src = DATA[index].song
             audio.setAttribute("id", "song" + (index + 1))
             button.innerHTML = "Play"
             button.classList.add('button')
@@ -103,19 +96,19 @@ const generateTable = () => {
             td.appendChild(button)
             return td
         },
-        1: (td, index) => {
-            td.innerHTML = DATA[index + 1].title
+        (td, index) => {
+            td.innerHTML = DATA[index].title
             return td
         },
-        2: (td, index) => {
-            td.innerHTML = DATA[index + 1].artist
+        (td, index) => {
+            td.innerHTML = DATA[index].artist
             return td
         },
-        3: (td, index) => {
-            td.innerHTML = DATA[index + 1].year
+        (td, index) => {
+            td.innerHTML = DATA[index].year
             return td
         },
-    }
+    ]
 
     for (let i = 0; i < Object.keys(DATA).length; i++) {
 
@@ -131,13 +124,12 @@ const generateTable = () => {
     }
 }
 
-
 generateTable()
 
 const BUTTONS = document.getElementsByClassName('button')
 const SONGS = document.getElementsByTagName('audio')
 
-CURRENT = null
+let CURRENT = null
 
 const handleStart = () => { // Simulate click on first Play button
 
@@ -175,7 +167,7 @@ const handlePlayPauseButtons = () => {
                 }
                 CURRENT = button
                 cover = document.getElementById('cover')
-                cover.src = DATA[i + 1].cover
+                cover.src = DATA[i].cover
                 button.innerHTML = 'Pause'
                 song.play()
             }
@@ -208,8 +200,9 @@ const handleEndOfSong = () => {
         })
     }
 }
-
-handlePlayPauseButtons()
-handleStart()
-handleRandom()
-handleEndOfSong()
+document.addEventListener('DOMContentLoaded', (event) => {
+    handlePlayPauseButtons()
+    handleStart()
+    handleRandom()
+    handleEndOfSong()
+})
