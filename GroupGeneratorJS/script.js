130c94e0529718d299dda91d09f763d52dec782e
Bart <bartlomiej.janik@hotmail.fr>
2021-01-18 18:27:50 +0100

    Works fine

--- GroupGeneratorJS/script.js
+++ GroupGeneratorJS/script.js
@@ -1,45 +1,79 @@
 const addButton = document.querySelector("#add")
+const delLastButton = document.querySelector("#del")
+const delAllButton = document.querySelector("#delAll")
 const genGroupsButton = document.querySelector('#genGroups')
+const delGroupsButton = document.querySelector('#delGroups')
+const names = document.querySelector("#names")
 
-addButton.addEventListener('click', () => {
-    const input = document.querySelector('input')
-    const list = document.querySelector("#listOfNames")
-    if (input.value.length > 0) {
-        const li = document.createElement('li')
-        li.innerHTML = input.value
-        list.appendChild(li)
-        input.value = ''
-    }
-})
+const nameRegex = new RegExp('^[A-Za-z]+$')
+const numberRegex = new RegExp('[0-9]+')
 
-const deleteAllGroups = container => {
+const deleteAllChildren = container => {
     while (container.firstChild) {
         container.removeChild(container.lastChild)
     }
 }
+
+addButton.addEventListener('click', () => {
+
+    const input = document.querySelector('input')
+    if (input.value.match(nameRegex)) {
+        if (names.childElementCount === 0) {
+            document.querySelector('#pool-heading').innerText = 'Pool'
+        }
+        const li = document.createElement('li')
+        li.innerText = input.value
+        names.appendChild(li)
+        input.value = ''
+    }
+})
+
+delLastButton.addEventListener('click', () => {
+    if (names.lastChild) {
+        names.removeChild(names.lastChild)
+    }
+    if (names.childElementCount === 0) {
+        document.querySelector('#pool-heading').innerText = ''
+    }
+})
+
+delAllButton.addEventListener('click', () => {
+    if (names.lastChild) {
+        deleteAllChildren(names)
+    }
+    document.querySelector('#pool-heading').innerText = ''
+})
+
+delGroupsButton.addEventListener('click', () => {
+    const groups = document.querySelector(".groups")
+
+    if (groups.firstChild)
+        deleteAllChildren(groups)
+
+})
 
 genGroupsButton.addEventListener('click', () => {
     const input = document.querySelector('#nbStudents')
     const nbStudents = parseInt(input.value)
     const container = document.querySelector(".groups")
 
-    if (container.firstChild)
-        deleteAllGroups(container)
-    
-    
-    if (input.value === '' || nbStudents < 1) {
+    if (!input.value.match(numberRegex) || nbStudents < 1) {
         return
     }
+    if (container.firstChild)
+        deleteAllChildren(container)
 
-    const lis = document.querySelector("#listOfNames").getElementsByTagName('li')
+    const lis = names.getElementsByTagName('li')
     let listOfNames = []
     for (let i = 0; i < lis.length; i++) {
         listOfNames.push(lis[i].textContent)
     }
     
+    let groupNumber = 1
     while (listOfNames.length > 0) {
         const div = document.createElement('div')
         const ul = document.createElement('ul')
+        const heading = document.createElement('h3')
         for (let i = 0; i < nbStudents; i++) {
             if (listOfNames.length === 0) {
                 break
@@ -50,7 +84,10 @@ genGroupsButton.addEventListener('click', () => {
             ul.appendChild(li)
             listOfNames.splice(index, 1)
         }
+        heading.innerText = "Group " + groupNumber
+        div.appendChild(heading)
         div.appendChild(ul)
         container.appendChild(div)
+        groupNumber += 1
     }
 })
